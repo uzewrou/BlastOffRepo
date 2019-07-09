b2f7cb81479430eb3f36a6397c05c2162b7fec28
uzewrou <uzewrou@gmail.com>
2019-07-09 12:10:57 -0700

    no message

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Transform PlayerTransform;
+
+    private Vector3 _cameraOffset;
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+
+    public bool LookAtPlayer = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    // LateUpdate is called after Update methods
+    void Update()
+    {
+        Vector3 newpos = PlayerTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newpos, SmoothFactor);
+
+        if (LookAtPlayer)
+            transform.LookAt(PlayerTransform);
+    }
+}
---
+++ Assets/Rock.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    Rigidbody rocketRB;
+    [SerializeField] AudioClip mainEngine;
+    AudioSource audioSource;
+
+    [SerializeField] float thrustSpeed;
+    [SerializeField] float RotationSpeed;
+    void Start()
+    {
+        rocketRB = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        Rotate();
+        Thrust();
+    }
+
+    // Update is called once per frame
+    void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rocketRB.AddRelativeForce(Vector3.up * thrustSpeed);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rocketRB.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(mainEngine);
+            }
+        }
+       else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    void Rotate()
+    {
+        rocketRB.freezeRotation = true;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * RotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back * RotationSpeed);
+        }
+        rocketRB.freezeRotation = false;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag) {
+            case "Friendly":
+                print("Your Okay");
+                break;
+            case "Finish":
+                print("You Win");
+                break;
+            case "Deathly":
+                print("YOU Dead");
+                break;
+        }
+    }
+
+
+
+
+
+
+
+}
